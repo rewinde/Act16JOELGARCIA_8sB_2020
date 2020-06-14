4df5cf52094575c74bebc7d75c8db4226860deae
rewinde <66702186+rewinde@users.noreply.github.com>
2020-06-14 15:52:41 -0500

    Add files via upload

---
+++ Act16JOELGARCIA_8sB_2020/Act16JOELGARCIA_8sB_2020.php
@@ -0,0 +1,77 @@
+<!DOCTYPE>
+<html>
+  <head>
+  <meta http-equiv= "Content-Type" content="text/html; charset=utf-8"/>
+<title> Tarea - JOEL JAVIER GACÍA ZAVÁLA  </title>
+<style type="text/css">
+.a{
+   color:#00F;
+   }
+  </style>
+  </head>
+
+<!DOCTYPE>
+<html>
+  <head>
+  <meta http-equiv= "Content-Type" content="text/html; charset=utf-8"/>
+<title> Tarea - JOEL JAVIER GACÍA ZAVÁLA  </title>
+<style type="text/css">
+.a{
+   color:#00F;
+   }
+  </style>
+  </head>
+
+  <body>
+   <div align="center">
+      <table width="200" border="1">
+     <tr>
+     <td width="54">Versace</td>
+     <td width="68"><a href="https://www.versace.com/"><img src="VER.JPG" width="68" height="68" > </a></td>
+      <?php $td="<td width='100'>AZUL</td>
+                        <td bgcolor='#115FC8' width='100'></td>" ;
+             echo $td;
+             ?>
+     </tr>
+
+     <tr>
+     <td width="54">DOCKERS</td>
+     <td width="68"><a href="https://www.dockers.com/"><img src="DOCKERS.JPG" width="68" height="68" > </a></td>
+      <?php $td="<td width='100'>AMARILLO</td>
+                        <td bgcolor='#FFDD35 ' width='100'></td>" ;
+             echo $td;
+             ?>
+     </tr>
+
+     <tr>
+     <td width="54">GAP</td>
+     <td width="68"><a href="https://www.gap.com/"><img src="GAP.JPG" width="68" height="68" > </a></td>
+     <?php $td1="<td width='100'>VERDE</td>
+               <td bgcolor='#00FF7F'></td>" ;
+             echo $td1;
+             ?>
+     </tr>
+
+     <tr>
+     <td width="54">Timberland</td>
+     <td width="68"><a href="https://www.timberland.es/"><img src="Timberland.JPG" width="68" height="68" > </a></td>
+      <?php $td1="<td width='100'>NEGRO</td>
+               <td bgcolor='#000000 '></td>" ;
+             echo $td1;
+             ?>
+     </tr>
+
+     <tr>
+     <td width="54">Vans</td>
+     <td width="68"><a href="https://www.vans.com/"><img src="Vans.JPG" width="68" height="68" > </a></td>
+      <?php $td1="<td width='100'>NARANJA</td>
+               <td bgcolor='#EC5C14  '></td>" ;
+             echo $td1;
+             ?>
+     </tr>
+
+
+
+  </body>
+
+</html>
